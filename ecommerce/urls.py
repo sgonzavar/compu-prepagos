b31d838b4123dc308ac7179157cd9d6c8353ead9
Tiago <sgonzalezvargas.7cc@gmail.com>
2022-11-08 10:49:02 -0500

    Feat: order elements for basic functionallity

---
+++ ecommerce/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+app_name = 'ecommerce'
+
+urlpatterns = [
+  path('', index, name='index'),
+  path('categories/<slug>', search_category, name='categories'),
+  path('search', search, name='search'),
+  path('detail/<slug>', detail, name='detail'),
+  path('<slug>/cart', cart, name='cart'),
+  path('mycart/', mycart, name='mycart'),
+]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,13 +3,13 @@ from .models import Category, Product
 from django.contrib import messages
 
 def index(request):
-  template_name = 'index.html'
+  template_name = 'templates/index.html'
   categories = Category.objects.filter(active=True)
   products = Product.objects.filter(active=True)
   context = {'products': products, 'categories': categories}
-  return render(request, template_name, context)
+  return render(request, "index.html", context)
 
-def searc_category(request, slug):
+def search_category(request, slug):
   template_name = 'list.html'
   cat = Category.objects.get(slug=slug)
   categories = Category.objects.filter(active=True)
@@ -19,13 +19,13 @@ def searc_category(request, slug):
 
 def search(request):
   template_name = 'list.html'
-  q = request.GET.get['q']
-  products = Product.objects.filter(active=True, name__icontains=q)
+  input = request.GET.get['input']
+  products = Product.objects.filter(active=True, name__icontains=input)
   categories = Category.objects.filter(active=True)
   context = {'products': products, 'categories': categories}
   return render(request, template_name, context)
 
-def datail(request, slug):
+def detail(request, slug):
   if Product.objects.filter(active=True, slug=slug).exists():
     template_name = 'datail.html'
     products = Product.objects.filter(active=True, slug=slug)
@@ -45,5 +45,14 @@ def cart(request, slug):
     session['price'] += float(product.price)
     session['count'] += 1
     request.session['data'] = session
-    messages.succes(request, 'Agragado con exito')
-  return redirect('ecommmerce:datail', slug=slug)
+    messages.succes(request, 'Agregado con exito')
+  return redirect('ecommmerce:detail', slug=slug)
+
+def mycart(request):
+  template_name = 'list.html'
+  session = request.session.get('data', {'items':[]})
+  products = Product.objects.filter(active=True, slug__in=session['items'])
+  categories = Category.objects.filter(active=True)
+  context = {'products': products, 'categories': categories}
+  return render(request, template_name, context)
+
